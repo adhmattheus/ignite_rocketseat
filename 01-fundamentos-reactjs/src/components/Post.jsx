292caba0ad042c5a40565419fe33bf4255d316f9
adhmattheus <mattheusadhonnaysf@hotmail.com>
2022-12-15 12:11:58 -0300

    componente avatar finalizado

--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
@@ -6,10 +7,9 @@ export function Post() {
 		<article className={styles.post}>
 			<header>
 				<div className={styles.author}>
-					<img
-						className={styles.avatar}
-						src="https://github.com/adhmattheus.png"
-					/>
+
+					<Avatar hasBorder src="https://github.com/adhmattheus.png" />
+
 					<div className={styles.authorInfo}>
 						<strong>Mattheus Lafera</strong>
 						<span>Web Developer</span>
