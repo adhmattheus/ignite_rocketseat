ca189fd0a7d29d7128e5a5164a0628a113237d24
adhmattheus <mattheusadhonnaysf@hotmail.com>
2022-12-15 09:30:36 -0300

    estilizando componente post

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,8 +1,8 @@
 import { Header } from './components/Header'
-import { Post } from './Post'
+import { Post } from './components/Post'
+import { Sidebar } from './components/Sidebar'
 import './global.css'
 import styles from './App.module.css'
-import { Sidebar } from './components/Sidebar'
 
 
 function App() {
@@ -13,6 +13,10 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
+          <Post
+            author="Lafera adhonnay"
+            content="texto pos"
+          />
           <Post
             author="Lafera adhonnay"
             content="texto pos"
---
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -0,0 +1,33 @@
+import styles from "./Post.module.css";
+
+export function Post() {
+	return (
+		<article className={styles.post}>
+			<header>
+				<div className={styles.author}>
+					<img
+						className={styles.avatar}
+						src="https://github.com/adhmattheus.png"
+					/>
+					<div className={styles.authorInfo}>
+						<strong>Mattheus Lafera</strong>
+						<span>Web Developer</span>
+					</div>
+				</div>
+				<time title="2022-15-12 08:00:10 " dataTime="2022-15-12 08:00:10"> Publicado há 1h </time>
+			</header>
+
+			<div className={styles.content}>
+				<p></p>Fala galeraa 👋
+				<p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+				<p>👉{' '}<a href="">jane.design/doctorcare</a></p>
+				<p>
+					<a href="">#novoprojeto </a>{' '}
+					<a href=""> #nlw </a>{' '}
+					<a href=""> #rocketseat</a>
+				</p>
+			</div>
+
+		</article>
+	);
+}
