3ed607c983ca929556cd388225011624e660dfa7
adhmattheus <mattheusadhonnaysf@hotmail.com>
2022-12-20 08:58:21 -0300

    dados formulario com useState

--- 01-fundamentos-reactjs/src/components/Comment.jsx
+++ 01-fundamentos-reactjs/src/components/Comment.jsx
@@ -2,24 +2,25 @@ import { ThumbsUp, Trash } from 'phosphor-react'
 import { Avatar } from './Avatar'
 import styles from './Comment.module.css'
 
-export function Comment() {
+export function Comment({ content }) {
 	return (
 		<div className={styles.comment}>
 			<Avatar hasBorder={false} src="https://github.com/adhmattheus.png" />
 
 			<div className={styles.commentBox}>
-
 				<div className={styles.commentContent}>
 					<header>
 						<div className={styles.authorAndTIme}>
 							<strong>Mattheus Adhonnay</strong>
 							<time title="2022-15-12 08:00:10 " dataTime="2022-15-12 08:00:10"> Cerca de1h atrás</time>
 						</div>
+
 						<button title="Dletar comentário">
 							<Trash size={24} />
 						</button>
 					</header>
-					<p>Muito bom Devon, parabéns!! 👏👏</p>
+
+					<p>{content}</p>
 				</div>
 
 				<footer>
--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,17 +1,39 @@
+import { useState } from "react";
 import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 import styles from "./Post.module.css";
-
 import ptBR from 'date-fns/locale/pt-BR';
-import { format } from "date-fns";
+import { format, formatDistanceToNow } from "date-fns";
 
 
+export function Post({ author, publishedAt, content }) {
 
-export function Post({ author, publishedAt }) {
+	const [comments, setComments] = useState([
+		'Post muito bom, ne não ? '
+	])
+
+	const [newCommentText, setNewCommentText] = useState('');
 
 	const publishedDateFormatted = format(publishedAt, "dd 'de' LLLL 'às' HH:mm'h'", {
 		locale: ptBR,
-	})
+	});
+
+	const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+		locale: ptBR,
+		addSuffix: true,
+	});
+
+	function handleCreateNewComent(e) {
+		e.preventDefault();
+
+		setComments([...comments, newCommentText]);
+		setNewCommentText('');
+
+	}
+
+	function handleNewCommentChange() {
+		setNewCommentText(event.target.value);
+	}
 
 	return (
 		<article className={styles.post}>
@@ -28,18 +50,30 @@ export function Post({ author, publishedAt }) {
 
 				</div>
 
-				<time title="" dataTime="2022-15-12 08:00:10">
-					{publishedDateFormatted}
+				<time title={publishedDateFormatted} dataTime={publishedAt.toISOString()}>
+					{publishedDateRelativeToNow}
 				</time>
-
 			</header>
 
+			<div className={styles.content}>
+				{content.map(line => {
+					if (line.type === 'paragraph') {
+						return <p>{line.content}</p>;
+					} else if (line.type === 'link') {
+						return <p> <a href="#">{line.content}</a></p>;
+					}
+				})}
+			</div>
 
-			<form className={styles.commentForm}>
+
+			<form onSubmit={handleCreateNewComent} className={styles.commentForm}>
 				<strong>Deixe seu feedback</strong>
 
 				<textarea
+					name="comment"
 					placeholder="Deixe um comentário"
+					value={newCommentText}
+					onChange={handleNewCommentChange}
 				/>
 
 				<footer>
@@ -48,10 +82,10 @@ export function Post({ author, publishedAt }) {
 			</form>
 
 			<div className={styles.commentList}>
-				<Comment />
-				<Comment />
+				{comments.map(comment => {
+					return <Comment content={comment} />
+				})}
 			</div>
-
 		</article>
 	);
 }
